e0175a2e98d3b62163d42e9798c00eede7425b45
Elena Tyshchenko <lenatishenko95@gmail.com>
2014-10-11 18:15:48 +0300

    Small change

--- views/admin/menu_bottom.php
+++ views/admin/menu_bottom.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+/* @var $form yii\widgets\ActiveForm */
 ?>
 <div><center><h3>Name хочет зарегестрироватся в роли Role</h3></center></div>
     <div class="line"></div>
@@ -9,14 +10,18 @@ use yii\widgets\ActiveForm;
    <div class="line"></div>
    <div>Information about user</div>
 <br></br>
+
    <div class="form-group">
    
         <?= Html::button('Принять', ['class' => 'btn btn-primary']) ?>
         <?= Html::button('Отклонить', ['class' => 'btn btn-primary']) ?> 
 <br></br>
-      <?= Html::textarea('Reason',null,['placeholder'=>'Причина'],['rows'=>'20']) ?>
+<div class="textar">
+ <?= Html::textarea('Reason',null,['placeholder'=>'Причина','cols'=>'27', 'rows'=>'7']) ?></div>
+     
 
 <br></br>
 	<div class="but-class">
         <?= Html::button('Отправить', ['class' => 'btn btn-primary']) ?></div>
+		
 </div>
