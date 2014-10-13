d84c390b1a30a665342101291c5b0717b2c107f1
Nikita Doyun <doyun.nikita@gmail.com>
2014-10-13 19:26:00 +0300

    Add files.

---
+++ views/student/st_update_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use app\models\Group;
+use yii\helpers\ArrayHelper;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Student */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="student-form">
+
+    <?php $form = ActiveForm::begin(['method' => 'post',]); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => 255]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput()?>
+
+    <?= $form->field($model, 'confirmation')->passwordInput()?>
+
+    <?= $form->field($model, 'idGroup')->dropDownList(ArrayHelper::map(Group::find()->all(), 'id', 'name'))->label('Group') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Update', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
