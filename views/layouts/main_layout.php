af38711b6ee730bbe71d8535cbade3110ca52b2e
Anastacia Zhadan <anastaciazh@gmail.com>
2014-11-04 21:44:14 +0200

    design changes

--- views/layouts/main_layout.php
+++ views/layouts/main_layout.php
@@ -25,7 +25,8 @@ AppAsset::register($this);?>
 		<?php $this->beginBody() ?>
 		<div class="wrap">
 			<div class="wrapper">
-				<div width="500px"><img src="../images/logo.png" alt="Coursey" title="Coursey"/></div>
+				<div width="500px"><img src="../images/logo.png" alt="Coursey" title="Coursey"/></div><br>
+				<div style="position:absolute; margin: -51px 0 0 15px;"><img src="../images/fold.png"/></div>
 				<?= $content ?>
 			</div>
 			<div><p class="credit" align="center">Copyright by Khnure Students. 2014<br></p></div>
